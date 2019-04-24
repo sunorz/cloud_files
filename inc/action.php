1887913939f73d042ae14c5809ee268c6b5733b4
sunorz <sunplace@live.cn>
2019-04-24 17:55:13 +0800

    Update action.php

--- inc/action.php
+++ inc/action.php
@@ -2,12 +2,12 @@
 /*
 Copyright by Sunplace
 CT:2019/1/11
-MT:2019/1/13
+MT:2019/4/24
 */
 session_start();
 if(isset($_POST['username'])&&isset($_POST['password'])){
 	require("functions.php");
-	if(ck($_POST['username'],md5($_POST['password'].'sunplace'))){
+	if(ck($_POST['username'],md5($_POST['password'].'CONFUSED_STRING'))){
 	$_SESSION['name']=$_POST['username'];
 	$_SESSION['password']=$_POST['password'];
 		echo 'success';
