716b22fbb1ad0b95004da61d6942995381cef5d3
sunorz <sunplace@live.cn>
2019-05-30 17:13:40 +0800

    Update upload.php

--- inc/upload.php
+++ inc/upload.php
@@ -4,7 +4,10 @@ Copyright by Sunplace
 CT:2019/1/7
 MT:2019/4/28
 */
+$t="";
+if(isset($_COOKIE['type'])){
 $t=$_COOKIE['type'];
+}
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
         header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
         header("Cache-Control: no-store, no-cache, must-revalidate");
